4e157fe3a10ddfb10db25adb0a8d63b5e2aa4f54
Sahand Yousefpour <sahand.yousefpour@gmail.com>
2016-03-06 04:15:50 +0100

    Major bugfixes + code cleaned + added comments

--- src/lepp3/util/VideoRecorder.hpp
+++ src/lepp3/util/VideoRecorder.hpp
@@ -15,7 +15,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-#include "lola/TransformObserver.hpp"
+#include "lola/PoseObserver.hpp"
 #include "lepp3/VideoObserver.hpp"
 
 #include "lepp3/debug/timer.hpp"
@@ -23,7 +23,7 @@
 
 namespace {
 /**
-* generate a name based on current timestamp which will be used to create a
+* Generate a name based on current timestamp which will be used to create a
 * directory.
 **/
 std::string get_dir_name() {
@@ -31,46 +31,36 @@ std::string get_dir_name() {
   time_t t = time(0);   // get time now
   struct tm * now = localtime( & t );
   ss << (now->tm_year + 1900) << '-'
-      << (now->tm_mon + 1) << '-'
-      <<  now->tm_mday << '_'
-      << now->tm_hour << now->tm_min << now->tm_sec;
+     << (now->tm_mon + 1) << '-'
+     <<  now->tm_mday << '_'
+     << now->tm_hour << now->tm_min << now->tm_sec;
   return ss.str();
-}
-
-std::ostream& operator<<(std::ostream& out, LolaKinematicsParams const& param) {
-  out << "stamp #" << param.stamp << std::endl
-      << "phi_z_odo = " << param.phi_z_odo << std::endl
-      << "stance = " << param.stance << std::endl;
-  out << "t_wr_cl = ";
-  for (int i = 0; i < 3; ++i) out << param.t_wr_cl[i] << " "; out << std::endl;
-
-  out << "R_Wr_cl = " << std::endl;
-  for (int i = 0; i < 3; ++i) {
-    out << "  ";
-    for (int j = 0; j < 3; ++j) {
-      out << param.R_wr_cl[i][j] << " ";
-    }
-    out << std::endl;
-  }
-  out << "t_stance_odo = ";
-  for (int i = 0; i < 3; ++i) out << param.t_stance_odo[i] << " "; out << std::endl;
-
-  return out;
 }
 
 } // namespace anonymous
 
-namespace lepp{
-  enum RecordingMode {
-    MODE_CLOUD = 0,
-    MODE_CLOUD_IMAGE,
-    MODE_CLOUD_IMAGE_POSE
-  };
-} // namespace lepp
-
 /**
-  * A recorder module that receives a point cloud and a pose (LolaKinematics)
-  * from the robot and saves them alongside each other for further offline use.
+  * A recorder module that receives a point cloud, an RGB image and a pose
+  * (LolaKinematics) from the camera and the robot and saves them alongside
+  * each other for further offline use.
+  *
+  * Since the publish rate of each of the above three elements are different,
+  * a lock-and-wait mechanism is applied to avoid data inconsistency.
+  *
+  * As the point cloud publisher is believed to be slowest, and the pose Service
+  * to be the fastest elements, a recording chain is assumed to save them
+  * accordingly.
+  *
+  * The order of saving the elements are
+  *     1. point cloud
+  *     2. rgb image
+  *     3. pose parameters
+  * Regardless of how many incoming frames we encounter, the chain has to be
+  * traversed completely each time, before saving a second instance of each
+  * element. This ensures the time drift between them to be minimum.
+  *
+  * Recording each of these elements are totally optional and could be set by
+  * the `setMode` method.
   */
 template<class PointT>
 class VideoRecorder : public VideoObserver<PointT>, public TFObserver {
@@ -91,48 +81,86 @@ public:
   void notifyNewFrame(int idx, const cv::Mat& image) {};
   /**
    * Implementation of the PoseObserver interface.
-  **/
-  virtual void notifyNewTF(int idx, const LolaKinematicsParams& params);
-  void setMode(lepp::RecordingMode mode);
+   */
+  virtual void NotifyNewPose(int idx, const LolaKinematicsParams& params);
+  /**
+   * Set different recording options.
+   * Determines whether to record the point cloud, rgb image and pose.
+   */
+  void setMode(bool cloud, bool rgb, bool pose);
 private:
+  /**
+   * Write the current point cloud on disk.
+   */
   void savePointCloud();
-  void writeParams(const LolaKinematicsParams& params);
+  /**
+   * Write the current pose parameters in file.
+   */
+  void saveParams();
+  /**
+   * Save the current image on disk.
+   */
   void saveImage();
-  lepp::RecordingMode rec_mode_;
+  /**
+   * Recording options
+   */
+  bool record_cloud_, record_rgb_, record_pose_;
+  /**
+   * Instance holding the current point cloud.
+   */
   typename pcl::PointCloud<PointT>::ConstPtr cloud_;
+  /**
+   * Instance holding the current RGB image.
+   */
   boost::shared_ptr<openni_wrapper::Image> image_;
+  /**
+   * Instance holding the current pose parameters.
+   */
+  LolaKinematicsParams params_;
+  /**
+   * System path to save the captured data.
+   */
   boost::filesystem::path path_;
-  std::string tf_file_name_;
-  int cloud_idx_;
-  int image_idx_;
-  int tf_idx_;
-  bool cloud_lk_, image_lk_, tf_lk_;
+  /**
+   * Name of the file which holds pose parameters.
+   */
+  std::string params_file_name_;
+  /**
+   * Internal indices to maintain the consistency between cloud, rgb and pose.
+   */
+  int cloud_idx_, image_idx_, params_idx_;
+  /**
+   *
+   */
+  bool cloud_lk_, image_lk_;
 };
 
 template<class PointT>
 VideoRecorder<PointT>::VideoRecorder()
   : path_( get_dir_name() ),
-    tf_file_name_("tf.txt"),
-    rec_mode_(MODE_CLOUD_IMAGE),
+    params_file_name_("params.txt"),
+    record_cloud_(true),
+    record_rgb_(false),
+    record_pose_(false),
     cloud_idx_(-1),
     image_idx_(-1),
-    tf_idx_(-1),
+    params_idx_(-1),
     cloud_lk_(false),
-    image_lk_(false),
-    tf_lk_(false) {
+    image_lk_(false) {
 
-  if(boost::filesystem::exists(path_)) {
-    if(boost::filesystem::is_directory(path_))
+  namespace bfs = boost::filesystem;
+  if(bfs::exists(path_)) {
+    if(bfs::is_directory(path_))
       std::cout << "directory " << path_ << "already exists. terminating...\n";
-      // TODO finish up the termination process
+      throw "No overwriting rule exists. Process terminated.";
   }
   else {
-    boost::filesystem::create_directory(path_);
-    //change current path to the new directory
-    boost::filesystem::current_path(path_);
+    bfs::create_directory(path_);
+    // change current path to the new directory
+    bfs::current_path(path_);
 
     // write header to tf_file
-    std::ofstream tf_fout(tf_file_name_.c_str());
+    std::ofstream tf_fout(params_file_name_.c_str());
     if(tf_fout.is_open()) {
       tf_fout << "# t_wr_cl[0],\tt_wr_cl[1],\tt_wr_cl[2],\t"
               << "R_wr_cl[0][0],\tR_wr_cl[0][1],\tR_wr_cl[0][2],\t"
@@ -144,25 +172,38 @@ VideoRecorder<PointT>::VideoRecorder()
       tf_fout.close();
     }
   }
+  std::cout << "exit ctor" << std::endl;
 }
 
 template<class PointT>
-void VideoRecorder<PointT>::setMode(lepp::RecordingMode mode) {
-  rec_mode_ = mode;
+void VideoRecorder<PointT>::setMode(bool cloud,
+                                    bool rgb,
+                                    bool pose) {
+  record_cloud_ = cloud;
+  record_rgb_ = rgb;
+  record_pose_ = pose;
 }
 
 template<class PointT>
 void VideoRecorder<PointT>::notifyNewFrame(
     int idx,
-  const typename pcl::PointCloud<PointT>::ConstPtr& cloud) {
-  // get the cloud and lock
-  if(!cloud_lk_) {
-    cloud_ = cloud;
-    cloud_idx_++;
-    cloud_lk_ = true;
+    const typename pcl::PointCloud<PointT>::ConstPtr& cloud) {
+
+  // Save the point cloud if
+  // 1. we are actually told to save it,
+  // 2. there is no previous cloud waiting for the completion of the recording
+  //    chain.
+  if (record_cloud_) {
+    if (!cloud_lk_) {
+      cloud_ = cloud;
+      cloud_idx_++;
+      savePointCloud();
+      // Set the cloud lock only if here is not the end of recording chain (if
+      // either rgb or pose is also going to be recorded)
+      if (record_pose_ || record_rgb_)
+        cloud_lk_ = true;
+    }
   }
-  else
-    std::cout << "cloud reader is still locked." << std::endl;
 }
 
 template<class PointT>
@@ -170,36 +211,58 @@ void VideoRecorder<PointT>::notifyNewFrame(
     int idx,
     const typename boost::shared_ptr<openni_wrapper::Image>& image) {
 
-  std::cout << "entered image callback" << std::endl;
-  if(cloud_lk_) {
-    image_idx_++;
-    image_ = image;
-    if(rec_mode_ = MODE_CLOUD_IMAGE) {
-      savePointCloud();
-      saveImage();
-      cloud_lk_ = false;
-      image_lk_ = false;
-      std::cout << "releasing cloud lock" << std::endl;
-      return;
-    }
-    image_lk_ = true;
-  }
+  std::cout << "IMAGE CALLBACK" << std::endl;
+  // Exception: Make the cloud lock ineffective, if we are not recording any
+  // point clouds.
+  if (!record_cloud_)
+    cloud_lk_ = true;
 
+  // Save the image if
+  // 1. we are actually told to save it,
+  // 2. there is already a [supposedly] saved point cloud.
+  // 3. there is no previous image waiting for the completion of the recording
+  //    chain.
+  if (record_rgb_) {
+    if(cloud_lk_ && !image_lk_) {
+      image_idx_++;
+      image_ = image;
+      saveImage();
+      // set the image lock only if we are recording pose...
+      if (record_pose_)
+        image_lk_ = true;
+      // ... otherwise, here is the end of recording chain. release all
+      // remaining locks.
+      else
+        cloud_lk_ = false;
+    }
+  }
 }
 
 template<class PointT>
-void VideoRecorder<PointT>::notifyNewTF(int idx, const LolaKinematicsParams& params) {
-  tf_idx_ = idx;
-  std::cout << "Recorder::tf_idx_ : " << idx << std::endl;
+void VideoRecorder<PointT>::NotifyNewPose(
+    int idx,
+    const LolaKinematicsParams& params) {
 
-  if (cloud_lk_) {
-    tf_idx_++;
-    // first, write the point cloud to disk
-    savePointCloud();
-    writeParams(params);
-    saveImage();
-    cloud_lk_ = false;
-    image_lk_ = false;
+  // Rare case: [DEBUG] Recorder only saves pose params. Make the cloud and
+  // image locks ineffective.
+  if (!record_cloud_ && !record_rgb_) {
+    cloud_lk_ = true;
+    image_lk_ = true;
+  }
+
+  // Save the parameters if
+  // 1. we are actually told to save them,
+  // 2. there is already a [supposedly] saved point cloud.
+  // 3. there is already a [supposedly] saved image.
+  if (record_pose_) {
+    if (cloud_lk_ && image_lk_) {
+      params_idx_++;
+      params_ = params;
+      saveParams();
+      // Release the locks for next frames.
+      cloud_lk_ = false;
+      image_lk_ = false;
+    }
   }
 }
 
@@ -233,8 +296,7 @@ void VideoRecorder<PointT>::saveImage() {
   if (1000 <= image_idx_ && image_idx_< 10000)
     ss << "image_" << image_idx_ << ".jpg";
   if (image_idx_ > 10000) {
-    std::cout << "reached max size for saving RGB images.";
-    return;
+    throw "reached max size for saving RGB images!";
   }
   const std::string file_name = ss.str();
 
@@ -244,26 +306,26 @@ void VideoRecorder<PointT>::saveImage() {
 }
 
 template<class PointT>
-void VideoRecorder<PointT>::writeParams(const LolaKinematicsParams& params) {
+void VideoRecorder<PointT>::saveParams() {
   Timer t;
   t.start();
   // open the file
   std::ofstream tf_fout_;
   // open the file and add the current params to the end of it
-  tf_fout_.open(tf_file_name_.c_str(), std::ofstream::app);
+  tf_fout_.open(params_file_name_.c_str(), std::ofstream::app);
   // write the parameters to the file
   std::stringstream ss;
-  for (size_t i = 0; i < 3; ++i) { ss << params.t_wr_cl[i] << "\t"; }
+  for (size_t i = 0; i < 3; ++i) { ss << params_.t_wr_cl[i] << "\t"; }
   for (size_t i = 0; i < 3; ++i) {
     for (size_t j = 0; j < 3; ++j) {
-      ss << params.R_wr_cl[i][j] << "\t";
+      ss << params_.R_wr_cl[i][j] << "\t";
     }
   }
-  for (size_t i = 0; i < 3; ++i) { ss << params.t_stance_odo[i] << "\t"; }
-  ss << params.phi_z_odo << "\t";
-  ss << params.stance << "\t";
-  ss << params.frame_num << "\t";
-  ss << params.stamp;
+  for (size_t i = 0; i < 3; ++i) { ss << params_.t_stance_odo[i] << "\t"; }
+  ss << params_.phi_z_odo << "\t";
+  ss << params_.stance << "\t";
+  ss << params_.frame_num << "\t";
+  ss << params_.stamp;
   tf_fout_ << ss.str() << std::endl;
   // and close the file...
   tf_fout_.close();
