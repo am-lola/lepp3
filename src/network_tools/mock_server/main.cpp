2203454ed3c438f3b4a6e7ecae88b76c1418ff95
ga59bul <tamas.bates@tum.de>
2016-07-15 23:23:12 +0200

    Update lola_mock_server to handle client disconnections The server now checks for disconnections from clients, and if a client drops the server will go back to waiting for a connection, allowing a new client to send it data without needing to restart the server.

--- src/network_tools/mock_server/main.cpp
+++ src/network_tools/mock_server/main.cpp
@@ -24,7 +24,7 @@ using am2b::iface::SurfaceMessage;
  * A tool for testing network communication between LEPP and other components
  *
  * This server will listen on a given port for a TCP connection from LEPP,
- * and once connected will receive and decode VisionMessages containing 
+ * and once connected will receive and decode VisionMessages containing
  * obstacles, surfaces, etc.
  *
  * This can be used to verify if data is being sent correctly, or as a reference
@@ -120,6 +120,87 @@ void failWithError(std::string s)
   exit(1);
 }
 
+void readDataFrom(int socket_remote, const sockaddr_in& si_other, bool verbose)
+{
+  unsigned char buf[BUFLEN];
+  while (1)
+  {
+    std::fill(buf, buf+BUFLEN, 0);
+    ssize_t headerSize = sizeof(VisionMessageHeader);
+    ssize_t total_received = 0;
+
+    if (verbose)
+    {
+      std::cout << "Waiting for VisionMessageHeader (" << headerSize << " bytes)..." << std::endl;
+    }
+
+    while (total_received < headerSize)
+    {
+      int recvd = 0;
+      recvd = read(socket_remote, &buf[total_received], BUFLEN-total_received);
+
+      if (recvd == 0) // connection died
+        return;
+      if (recvd == -1) // failed to read from socket
+        failWithError("read() failed!");
+
+      total_received += recvd;
+
+      if (verbose)
+      {
+        printf("(header) Received %zu total bytes from %s:%d\n", total_received, inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+      }
+    }
+
+    VisionMessageHeader* header = (VisionMessageHeader*)buf;
+
+    std::cout << "Received VisionMessageHeader: " << *header << std::endl;
+
+    if (verbose)
+    {
+      std::cout << "Waiting to receive a total of " << header->len + sizeof(VisionMessageHeader) << " bytes..." << std::endl;
+    }
+
+    while (total_received < header->len + sizeof(VisionMessageHeader))
+    {
+      int recvd = 0;
+      recvd = read(socket_remote, &buf[total_received], BUFLEN-total_received);
+
+      if (recvd == 0) // connection died
+        return;
+      if (recvd == -1) // failed to read from socket
+        failWithError("read() failed!");
+
+      total_received += recvd;
+      if (verbose)
+      {
+        printf("(message) Received %zu total bytes from %s:%d\n", total_received, inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+      }
+    }
+
+    switch (header->type)
+    {
+      case Message_Type::Obstacle:
+      {
+        ObstacleMessage* message = (ObstacleMessage*)(buf+sizeof(VisionMessageHeader));
+        std::cout << "Received obstacle: " << std::endl;
+        std::cout << *message << std::endl;;
+        break;
+      }
+      case Message_Type::Surface:
+      {
+        std::cout << "Received Surface" << std::endl; /// TODO: Fill in surface data once it's being sent from LEPP
+        break;
+      }
+      default:
+      {
+        std::cout << "UNKNOWN message type: " << header->type << "!!" << std::endl;
+      }
+    }
+
+  }
+}
+
 void listen(unsigned int port, bool verbose)
 {
   struct sockaddr_in si_me, si_other;
@@ -139,96 +220,22 @@ void listen(unsigned int port, bool verbose)
   if (listen(s, 5) != 0)
     failWithError("listen failed!");
 
-  std::cout << "Socket opened, listening for data on port " << port << "..." << std::endl;
-  
-  s_other = accept(s, (struct sockaddr* ) &si_other, &slen);
-  if (s_other < 0)
-    failWithError("accept failed!");
-
-
-  unsigned char buf[BUFLEN];
-  while (1)
+  while(1)
   {
-    std::fill(buf, buf+BUFLEN, 0);
-    ssize_t headerSize = sizeof(VisionMessageHeader);
-    ssize_t total_received = 0;
+    std::cout << "Listening for connection on port " << port << "..." << std::endl;
 
-    if (verbose)
-    {
-      std::cout << "Waiting for VisionMessageHeader (" << headerSize << " bytes)..." << std::endl;
-    }
+    s_other = accept(s, (struct sockaddr* ) &si_other, &slen);
+    if (s_other < 0)
+      failWithError("accept failed!");
 
-    while (total_received < headerSize)
-    {
-      int recvd = 0;
-      recvd = read(s_other, &buf[total_received], BUFLEN-total_received);
+    // receive data from new connection
+    readDataFrom(s_other, si_other, verbose);
 
-      if (recvd == -1)
-        failWithError("read() failed!");
-
-      total_received += recvd;
-
-      if (verbose)
-      {
-        printf("(header) Received %zu total bytes from %s:%d\n", total_received, inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-      }
-    }
-
-    VisionMessageHeader* header = (VisionMessageHeader*)buf;
-
-    std::cout << "Received VisionMessageHeader:" << std::endl;
-    std::cout << "\t type:" << header->type << ", len:" << header->len << std::endl;
-
-    if (verbose)
-    {
-      std::cout << "Waiting to receive a total of " << header->len + sizeof(VisionMessageHeader) << " bytes..." << std::endl;
-    }
-
-    while (total_received < header->len + sizeof(VisionMessageHeader))
-    {
-      int recvd = 0;
-      recvd = read(s_other, &buf[total_received], BUFLEN-total_received);
-      if (recvd == -1)
-        failWithError("read() failed!");
-
-      total_received += recvd;
-      if (verbose)
-      {
-        printf("(message) Received %zu total bytes from %s:%d\n", total_received, inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-      }
-    }
-
-    switch (header->type)
-    {
-      case Message_Type::Obstacle:
-      {
-        ObstacleMessage* message = (ObstacleMessage*)(buf+sizeof(VisionMessageHeader));
-        std::cout << "Received obstacle:" << std::endl;
-        std::cout << "\t Type: " << message->type << " ID: " << message->model_id << "|" << message->part_id << std::endl;
-        std::cout << "\t Action: 0x" << std::hex << message->action << std::dec << ", radius: " << message->radius <<  ", surface: " << message->surface << std::endl;
-        std::cout << "\t Coefficients: [";
-        for (int i = 0; i < 9; i++)
-        {
-          std::cout << message->coeffs[i];
-          if (i < 8)
-            std::cout << ", ";
-        }
-        std:: cout << "]" << std::endl;
-        break;
-      }
-      case Message_Type::Surface:
-      {
-        std::cout << "Received Surface" << std::endl; /// TODO: Fill in surface data once it's being sent from LEPP
-        break;
-      }
-      default:
-      {
-        std::cout << "UNKNOWN message type: " << header->type << "!!" << std::endl;
-      }
-    }
-
+    std::cout << "Connection to client terminated!" << std::endl;
+    std::cout << "-------------------------------------" << std::endl << std::endl;;
+    close(s_other);
   }
-  close(s_other);
+
   close(s);
 }
 
@@ -245,4 +252,3 @@ int main(int argc, char* argv[])
 
   return 0;
 }
-
