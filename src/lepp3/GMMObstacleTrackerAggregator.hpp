6d29a12c58114850326f7c622cb103a2a18f6644
Sahand <sahand.yousefpour@gmail.com>
2016-10-28 18:53:40 +0200

    minor changes in function args

--- src/lepp3/GMMObstacleTrackerAggregator.hpp
+++ src/lepp3/GMMObstacleTrackerAggregator.hpp
@@ -1,7 +1,7 @@
 #ifndef LEPP3_GMM_OBSTACLE_TRACKER_AGGREGATOR_H__
 #define LEPP3_GMM_OBSTACLE_TRACKER_AGGREGATOR_H__
 
-#include "lepp3/obstacles_new/ObstacleTrackerState.hpp"
+#include "lepp3/GMMObstacleTrackerState.hpp"
 
 namespace lepp
 {
@@ -10,13 +10,19 @@ namespace lepp
  * An interface that all classes that wish to be notified of the result generated
  * by GMMObstacleTracker need to implement.
  */
-template<class PointT>
 class GMMObstacleTrackerAggregator {
 public:
   virtual void updateObstacleTrackingData(
     ar::PointCloudData const& cloud_data,
     VoxelGrid const& vg,
     GMM::RuntimeStat runtime_stats) = 0;
+
+  /**
+   * Notify any attached observer of each of the obstacle states (according to
+   * the kalman filter).
+   */
+  virtual void updateObstacleState(
+    GMM::State& state, GMM::VisModifyFlag const& flag) = 0;
 };
 
 } // namespace lepp
