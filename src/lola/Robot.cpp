642028036da7e2bc001cc3f1a09c7181136fb32c
Stefan Floeren <steef@steef389.eu>
2017-01-12 15:05:53 +0100

    Fix out of bounds access for issue #34

--- src/lola/Robot.cpp
+++ src/lola/Robot.cpp
@@ -2,6 +2,8 @@
 #include "lepp3/models/Coordinate.h"
 #include "lepp3/models/SurfaceModel.h"
 
+#include <Eigen/Geometry>
+#include <limits>
 
 using namespace lepp;
 
@@ -26,51 +28,45 @@ bool Robot::isInRobotBoundary(SurfaceModel const& model) const {
 
   Coordinate robot_position = this->robot_position();
   PointT robot_center;
-  robot_center.x=robot_position.x;
-  robot_center.y=robot_position.y;
-  robot_center.z=robot_position.z;
+  robot_center.x = robot_position.x;
+  robot_center.y = robot_position.y;
+  robot_center.z = robot_position.z;
 
   // check if robot is inside (or standing on) the surface
   bool isInside = pcl::isPointIn2DPolygon(robot_center, *(model.get_hull()));
+  if (isInside) {
+    return true;
+  }
 
   // check if robot is close to the edge of the surface
-  double sq_min_dist_to_poly = 10000000;
-  if(!isInside)
-  {
-    for(int i = 1; i <= model.get_hull()->points.size(); i++)
-    {
-      double sq_dist_to_poly = 10000000; // distance to this edge
-      PointT point1 = model.get_hull()->points[i];
-      PointT point2 = model.get_hull()->points[i-1];
+  double sq_min_dist_to_poly = std::numeric_limits<double>::max();
 
-      Eigen::Vector3d p1_to_p2 = {(point2 - point1).x, (point2 - point1).y, (point2 - point1).z};
-      Eigen::Vector3d p1_to_robot = {(robot_center - point1).x, (robot_center - point1).y, (robot_center - point1).z}; // I hate myself
-      Eigen::Vector3d p2_to_robot = {(robot_center - point2).x, (robot_center - point2).y, (robot_center - point2).z}; // I hate myself
+  const size_t hull_point_count = model.get_hull()->points.size();
+  for (size_t i = 1; i <= hull_point_count; ++i) {
+    PointT const& point1 = model.get_hull()->points[i % hull_point_count];
+    PointT const& point2 = model.get_hull()->points[i - 1];
 
-      auto r = p1_to_robot.dot(p1_to_p2/p1_to_p2.norm()); //scalar projection of p1_to_robot into p1_to_p2
-      r /= p1_to_p2.norm(); //this checks if the projection of p1_to_robot into p1_to_p2 lies inside or outside the segment
+    Eigen::Vector3d p1_to_p2 = {(point2 - point1).x, (point2 - point1).y, (point2 - point1).z};
+    Eigen::Vector3d p1_to_robot = {(robot_center - point1).x, (robot_center - point1).y, (robot_center - point1).z}; // I hate myself
+    Eigen::Vector3d p2_to_robot = {(robot_center - point2).x, (robot_center - point2).y, (robot_center - point2).z}; // I hate myself
 
+    auto r = p1_to_robot.dot(p1_to_p2 / p1_to_p2.norm()); //scalar projection of p1_to_robot into p1_to_p2
+    r /= p1_to_p2.norm(); //this checks if the projection of p1_to_robot into p1_to_p2 lies inside or outside the segment
 
-      if (r < 0) // robot lies outside of the segment, closer to p1
-      {
-        sq_dist_to_poly = p1_to_robot.squaredNorm();
-      }
-      else if (r > 1) // robot lies outside of the segment, closer to p2
-      {
-        sq_dist_to_poly = p2_to_robot.squaredNorm();
-      }
-      else // robot lies inside the segment
-      {
-        sq_dist_to_poly = p1_to_robot.squaredNorm()
-                            - r * p1_to_p2.squaredNorm();
-      }
-
-      sq_min_dist_to_poly = std::min(sq_dist_to_poly, sq_min_dist_to_poly);
+    double sq_dist_to_poly = 0; // distance to this edge
+    if (r < 0) { // robot lies outside of the segment, closer to p1
+      sq_dist_to_poly = p1_to_robot.squaredNorm();
+    } else if (r > 1) { // robot lies outside of the segment, closer to p2
+      sq_dist_to_poly = p2_to_robot.squaredNorm();
+    } else { // robot lies inside the segment
+      sq_dist_to_poly = p1_to_robot.squaredNorm() - r * p1_to_p2.squaredNorm();
     }
+
+    sq_min_dist_to_poly = std::min(sq_dist_to_poly, sq_min_dist_to_poly);
   }
 
   // The surface is considered to be in the robot's boundary if closer
   // than a particular threshold or the robot's position is within the
   // bounds of the surface's convex hull.
-  return isInside || (sq_min_dist_to_poly < inner_zone_square_radius_);
+  return sq_min_dist_to_poly < inner_zone_square_radius_;
 }
