am-lola/lepp3

commit 456f32ca0ab02125c3bbbe4967da186c0e94ab59

Construct list of sorted filenames for pcd playback instead of just using a directory name This ensures all the PCDs are sorted in the correct order (sometimes PCD Grabber would load them somewhat arbitrarily) and that files used for pcd playback are sorted and loaded in the same order as files used for am_offline playback.

src/config/FileConfigParser.hpp
@@ -149,9 +149,11 @@ protected:
 
     } else if (type == "pcd") {
       const std::string file_path = getTomlValue<std::string>(toml_tree_, "VideoSource.file_path");
+      FileManager fm(FileManager::expandEnvironmentVars(file_path));
+      const std::vector<std::string> file_names = fm.getFileNames(".pcd");
       boost::shared_ptr<pcl::Grabber> interface(new pcl::PCDGrabber<PointT>(
-          FileManager::expandEnvironmentVars(file_path),
-          20.f,
+          file_names,
+          30.0f,
           true));
       this->raw_source_ = boost::shared_ptr<VideoSource<PointT>>(
           new GeneralGrabberVideoSource<PointT>(interface, this->pose_service()));

commit 30863f174ceddd0a9424a3a018a960f7efd51eb5

Restore support for single PCDs in config files (regressed in 456f32ca0ab02125c3bbbe4967da186c0e94ab59 ) This ensures that we can now process both directories of PCDs (to playback moving scenes) as well as individual PCDs (to playback static scenes)

src/config/FileConfigParser.hpp
@@ -148,9 +148,20 @@ protected:
           new LiveStreamSource<PointT>(this->pose_service(), enable_rgb));
 
     } else if (type == "pcd") {
-      const std::string file_path = getTomlValue<std::string>(toml_tree_, "VideoSource.file_path");
-      FileManager fm(FileManager::expandEnvironmentVars(file_path));
-      const std::vector<std::string> file_names = fm.getFileNames(".pcd");
+      const std::string file_path = FileManager::expandEnvironmentVars(getTomlValue<std::string>(toml_tree_, "VideoSource.file_path"));
+      std::vector<std::string> file_names;
+
+      // if input is a single PCD, just use it
+      if (boost::algorithm::ends_with(file_path, ".pcd"))
+      {
+          file_names.push_back(file_path);
+      }
+      // if input is a directory of PCDs, collect them all
+      else
+      {
+          FileManager fm(file_path);
+          file_names = fm.getFileNames(".pcd");
+      }
       boost::shared_ptr<pcl::Grabber> interface(new pcl::PCDGrabber<PointT>(
           file_names,
           30.0f,